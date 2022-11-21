c0d894ec408fdb79527eed78138d9be4673edbf7
Andrii Katrych <80524010+Glitch458@users.noreply.github.com>
2022-11-21 14:16:47 +0200

    fix

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,14 +1,14 @@
-const RequestError = require('../helpers/RequestError');
+const RequestError = require("../helpers/RequestError");
 
-const validateBody = schema => {
-    const validateFunction = (req, res, next) => {
-        const { error } = schema.validate(req.body);
-        if (error) {
-            next(RequestError(400, 'missing required name field'));
-        }
-        next();
+const validateBody = (schema) => {
+  const validateFunction = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      next(RequestError(400, error.message));
     }
-    return validateFunction;
-}
+    next();
+  };
+  return validateFunction;
+};
 
 module.exports = validateBody;
