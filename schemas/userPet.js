8ce88a90a39ffc30133a5014f6dc7daec4537c7f
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2022-11-23 01:46:01 +0200

    fix validation userPetSchema

--- schemas/userPet.js
+++ schemas/userPet.js
@@ -1,10 +1,16 @@
 const Joi = require("joi");
 
 const addUserPetSchema = Joi.object({
-  name: Joi.string(),
-  date: Joi.string(),
-  breed: Joi.string(),
-  coments: Joi.string(),
+  name: Joi.string()
+    .regex(/^[a-zA-Z]+$/)
+    .min(2)
+    .max(16),
+  date: Joi.date(),
+  breed: Joi.string()
+    .regex(/^[a-zA-Z]+$/)
+    .min(2)
+    .max(16),
+  coments: Joi.string().min(18).max(120),
   photoURL: Joi.string(),
 });
 
